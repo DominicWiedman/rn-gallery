4deb7fefe1620c45b7e886deafdfc483a1363a0a
Nikolay Dolgushin <dolgushin111@yandex.ru>
2019-06-02 12:48:41 +0700

    add loaders and infinity scroll

--- App.js
+++ App.js
@@ -6,22 +6,21 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {
     Platform,
     StyleSheet,
-    Text,
     View,
     FlatList,
     ActivityIndicator,
     Image,
     Modal,
+    Button,
     TouchableOpacity,
-    CameraRoll
-} from 'react-native';
-import {RNCamera, FaceDetector} from 'react-native-camera';
 
-export default class App extends Component {
+} from 'react-native';
+
+export default class App extends React.Component {
 
     constructor() {
         super();
@@ -38,59 +37,46 @@ export default class App extends Component {
 
     componentDidMount(): void {
         this.getData()
+
     }
 
 
-    async getData() {
-        // this.loader()
-        const url = `https://webapi.500px.com/discovery/fresh?rpp=20&feature=fresh&image_size%5B%5D=31&page=${this.state.page}`
+    getData = async () => {
+        this.loader()
+        let url = `https://webapi.500px.com/discovery/fresh?rpp=10&feature=fresh&image_size%5B%5D=3&image_size%5B%5D=36&page=${this.state.page}`
         await fetch(url)
             .then((response) => response.json())
             .then((responseJson) => {
                 this.setState({
                     data: [...this.state.data, ...responseJson.photos],
                     loading: false,
-                    refreshing: false
                 })
             })
             .catch((error) => {
                 console.error(error);
             })
-
-
     }
 
-    //newPage = () => {
-        // this.setState({
-        //     page: this.state.page + 1
-        // },
-        //    await this.getData()
-        // )
-    //}
+    newPage = async () => {
+        this.setState({
+                page: this.state.page + 1
+            }, () => {
+                this.getData()
+            }
+        )
+    }
 
-    // refresh() {
-    //     this.setState({
-    //         page: 1,
-    //         refreshing: true,
-    //     }, () => {
-    //         this.getData()
-    //     })
-    // }
+    loader() {
+        if (this.state.loading) {
+            return null;
+        }
+        return (
+            <View style={styles.loader}>
+                <ActivityIndicator size='large' color='#fff'/>
+            </View>
+        )
 
-    // loader = () => {
-    //     if (!this.state.loading) {
-    //         return null;
-    //     }
-    //     return (
-    //         <View style={styles.loader}>
-    //             <ActivityIndicator animating size="large"/>
-    //         </View>
-    //     )
-    // }
-
-    // separator = () => {
-    //
-    // }
+    }
 
     ShowModal(visible, imageURL) {
         this.setState({
@@ -105,18 +91,21 @@ export default class App extends Component {
             <View style={styles.container}>
                 <FlatList data={this.state.data}
                           renderItem={({item}) =>
-                              <TouchableOpacity onPress={this.ShowModal.bind(this, true, item.images[0].url)}
-                                                style={{flex: 1, flexDirection: 'column',}}>
-                                  <Image style={styles.image} source={{uri: item.images[0].url}}/>
+                              <TouchableOpacity
+                                  onPress={this.ShowModal.bind(this, true, item.images[1].url)} // item.image[1] get large image
+                                  style={{flex: 1, flexDirection: 'column',}}>
+                                  <Image style={styles.image}
+                                         source={{uri: item.images[0].url}}  // item.image[0] get large image
+                                  />
                               </TouchableOpacity>
                           }
                           numColumns={2}
                           keyExtractor={(item, index) => index}
-                          // onEndReached={this.newPage()}
-                          // onEndReachedThreshold={0}
-
-
-                />
+                          ListFooterComponent={this.loader()}
+                          onEndReachedThreshold={3}
+                          onEndReached={({}) => {
+                              this.newPage()
+                          }}/>
                 {
                     this.state.ModalVisibleStatus ? (
                         <Modal transparent={false} visible={this.state.ModalVisibleStatus} animationType={"fade"}
@@ -125,18 +114,15 @@ export default class App extends Component {
                                }}>
                             <View style={styles.modal}>
                                 <Image style={styles.modalImage} source={{uri: this.state.SelectImage}}/>
-                                <TouchableOpacity actveOpacity={0.5} style={styles.closeButton} onPress={() => {
-                                    this.ShowModal(!this.state.ModalVisibleStatus)
-                                }}>
-                                    <Image style={{width: 25, height: 25}}
-                                           source={{uri: 'https://reactnativecode.com/wp-content/uploads/2018/01/close_button.png'}}/>
+                                <TouchableOpacity actveOpacity={0.5} style={styles.closeButton}>
+                                    <Button title='Close' onPress={() => {
+                                        this.ShowModal(!this.state.ModalVisibleStatus)
+                                    }}/>
                                 </TouchableOpacity>
                             </View>
-
                         </Modal>
                     ) : null
                 }
-
             </View>
         );
     }
@@ -144,18 +130,16 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        marginTop: Platform.OS === 'ios' ? 20 : 0,
         flex: 1,
         flexDirection: 'column',
         backgroundColor: 'black',
     },
-    item: {},
     image: {
         height: 200,
     },
     loader: {
-        paddingVertical: 20,
-        borderTopWidth: 1,
-        borderColor: "#d0004a"
+        paddingVertical: 80
     },
     modal: {
         flex: 1,
@@ -167,14 +151,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         height: '100%',
-        width: '98%',
+        width: '100%',
         resizeMode: 'contain'
     },
     closeButton: {
         top: 9,
         left: 9,
-        width: 25,
-        height: 25,
-        position: 'absolute'
+        position: 'absolute',
     }
 });
